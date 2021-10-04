aade38a692e09f1d3363d42db66c6fee712ae3c1
Negarnbkhti42 <norob35@gmail.com>
2021-10-04 08:50:57 +0330

    project template

---
+++ compiler.py
@@ -0,0 +1,24 @@
+# Negar Nobakhti 98171201
+# Neda Taghizadeh Serajeh
+
+LETTER = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+DIGIT = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+KEYWORD = ['if', 'else', 'void', 'int', 'repeat', 'break', 'until', 'return']
+SYMBOL = [';', ':', ',', '[', ']', '{', '}', '(', ')', '+', '-', '*', '=', '<']
+SLASH = '/'
+STAR = '*'
+WHITESPACE = [32, 10, 13, 9, 11, 12]
+
+input = open('input.txt', 'r')
+lines = input.readlines()
+
+lineno = 1
+
+while lines:
+    get_next_token()
+
+def get_next_token():
+    text = lines[0]
