3d07e657b05f4173950326a20cea4a52f8b86a7f
Negarnbkhti42 <norob35@gmail.com>
2021-10-17 09:53:32 +0330

    add docstring

--- compiler.py
+++ compiler.py
@@ -1,10 +1,15 @@
-# Negar Nobakhti 98171201
-# Neda Taghizadeh Serajeh 98170743
+'''
+compiler design project
+
+group members:
+    Negar Nobakhti 98171201
+    Neda Taghizadeh Serajeh 98170743
+'''
 
 LETTER = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
           'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
-                                                                      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+          'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
           'X', 'Y', 'Z']
 DIGIT = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 KEYWORD = ['if', 'else', 'void', 'int', 'repeat', 'break', 'until', 'return']
