1fad76579a195ff209caa44cbe1225ffec68d0aa
Negarnbkhti42 <norob35@gmail.com>
2021-11-11 13:31:37 +0330

    seperate scanner

--- compiler.py
+++ compiler.py
@@ -5,197 +5,11 @@ group members:
     Negar Nobakhti 98171201
     Neda Taghizadeh Serajeh 98170743
 '''
-
-LETTER = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-DIGIT = set("0123456789")
-KEYWORD = ['if', 'else', 'void', 'int', 'repeat', 'break', 'until', 'return']
-SYMBOL = [';', ':', ',', '[', ']', '{', '}', '(', ')', '+', '-', '*', '=', '<']
-COMMENT = ['/', '*', '//', '/*', '*/']
-WHITESPACE = set(" \n\r\v\t\f")
+import scanner
 
 RECOGNIZED_IDS = []
 TOKENS = {}
 ERRORS = {}
-
-inputFile = open('input.txt', 'r')
-
-line_num = 1
-pointer_position = 0
-    
-def get_char():
-    ''' Reads one character from file, returns character and position '''
-    global pointer_position
-
-    pointer_position += 1
-    char = inputFile.read(1)
-    if char == '\n':
-        pointer_position += 1
-    return char
-
-
-def move_pointer(offset):
-    ''' returns back in file '''
-    global pointer_position
-
-    pointer_position += offset
-    inputFile.seek(pointer_position)
-    
-
-
-def skip_whitespace_and_comment():
-    ''' for skipping unneeded whitespaces and comments '''
-    
-    global line_num
-
-    while True:
-        value = get_char()
-        if value in WHITESPACE:
-            if value == '\n':
-                line_num += 1
-
-
-        elif value == COMMENT[0]:
-            value += get_char()
-
-            if value == '//':
-                value = get_char()
-
-                while value != '\n' and value != '':
-                    value = get_char()
-
-                if value == '\n':
-                    line_num += 1
-            elif value == '/*':
-                start_line = line_num
-                input_char = get_char()
-
-                while input_char != '':
-                    value += input_char
-                    if input_char == '\n':
-                        line_num += 1
-                    elif input_char == '*':
-                        input_char = get_char()
-
-                        if input_char == "/":
-                            break
-                        elif input_char == '\n':
-                            value+= input_char
-                            line_num += 1
-
-                    input_char = get_char()
-                if input_char == '':
-                    return (False, 'Unclosed comment', value, start_line)
-            else:
-                move_pointer(-1)
-                return (False, 'Invalid input', value[0])
-        else:
-            return value
-
-
-def get_id(value):
-    ''' returns IDs and KEYWORDs '''
-
-    input_char = get_char()
-    while input_char != '':
-
-        if input_char in LETTER or input_char in DIGIT:
-            value += input_char
-        elif input_char in SYMBOL or input_char in WHITESPACE:
-            move_pointer(-1)
-            return (True, 'KEYWORD' if value in KEYWORD else 'ID', value)
-        elif input_char == COMMENT[0]:
-            input_char += get_char()
-
-            if input_char in COMMENT:
-                move_pointer(-2)
-                return (True, 'KEYWORD' if value in KEYWORD else 'ID', value)
-            else:
-                value += input_char[0]
-                move_pointer(-1)
-                return (False, 'Invalid input', value)
-        else:
-            value += input_char
-            return (False, 'Invalid input', value)
-        input_char = get_char()
-    return (True, 'KEYWORD' if value in KEYWORD else 'ID', value)
-
-
-def get_num(value):
-    ''' returns NUMs '''
-
-    input_char = get_char()
-    # biad adad haro biabe
-
-    while input_char in DIGIT:
-        value += input_char
-        input_char = get_char()
-
-    if input_char in LETTER:
-        value += input_char
-        return (False, 'Invalid number', value)
-
-    if input_char != '':
-        move_pointer(-1)
-    return (True, 'NUM', value)
-
-
-def get_symbol(value):
-    ''' returns SYMBOLs '''
-
-    if value=="=":
-        input_char = get_char()
-
-        if input_char == "=":
-            value += input_char
-            return (True, 'SYMBOL', value)  # ==
-        if input_char in LETTER or input_char in DIGIT or input_char in WHITESPACE or input_char in COMMENT:
-            move_pointer(-1)
-            return (True, 'SYMBOL', value)  # =
-        value += input_char
-        return (False, 'Invalid input', value)
-    elif value == '*':
-        input_char = get_char()
-
-        if input_char == '/':
-            value += input_char
-            return (False, 'Unmatched comment', value)
-        elif input_char in LETTER or input_char in DIGIT or input_char in WHITESPACE:
-            # move_pointer(-1)
-            return (True, 'SYMBOL', value)
-        else:
-            value += input_char
-            return (False, 'Invalid input', value)
-    else:
-        return (True, 'SYMBOL', value)  # harchizi joz   ==  va  =
-
-
-def get_next_token():
-    ''' the ultimate function for finding tokens '''
-
-    value = skip_whitespace_and_comment()
-
-    if isinstance(value, tuple):
-        return value
-
-    if value == '':
-        return None
-
-    # ID/KEYWORD////////////////////
-    if value in LETTER:
-        return get_id(value)
-
-    # NUM///////////////////////////
-
-    if value in DIGIT:
-        return get_num(value)
-
-    # SYMBOL//////////////////////
-
-    if value in SYMBOL:
-        return get_symbol(value)
-
-    return (False, 'Invalid input', value)
-
 
 def add_token_to_dict(line, token_type, token_value):
     ''' adds valid tokens to the dict object '''
@@ -244,19 +58,20 @@ def add_symbols_to_table():
 
     with open('symbol_table.txt', 'w+') as symbol_file:
         index = 1
-        for keyword in KEYWORD:
+        for keyword in scanner.KEYWORD:
             symbol_file.write(f"{index}.\t{keyword}\n")
             index += 1
         for identifier in RECOGNIZED_IDS:
             symbol_file.write(f"{index}.\t{identifier}\n")
             index += 1
 
+scanner.openFile('input.txt')
 
 while True:
-    token = get_next_token()
+    token = scanner.get_next_token()
     if token:
         if token[0]:
-            add_token_to_dict(line= line_num,token_type= token[1], token_value= token[2])
+            add_token_to_dict(line= scanner.line_num,token_type= token[1], token_value= token[2])
 
         else:
             line = 0
@@ -268,16 +83,16 @@ while True:
                 else:
                     value = token[2]
             else:
-                line = line_num
+                line = scanner.line_num
                 value = token[2]
-                
+
             addErrorToDict(line= line, error_type=token[1], error_value=value)
 
 
     else:
         break
 
-inputFile.close()
+scanner.close_file()
 
 add_tokens_to_file()
 addErrorToFile()
