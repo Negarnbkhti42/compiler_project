d26bfd79640971a324ea2615388a996272bf43a3
Negarnbkhti42 <norob35@gmail.com>
2021-12-06 10:45:46 +0330

    mandatory rename

--- compiler.py
+++ compiler.py
@@ -24,7 +24,7 @@ class Diagram:
     def __init__(self, procedure):
         self.procedure = procedure
         self.branch = self.get_branch()
-        self.level = 0
+        self.state = 0
 
     def get_branch(self):
         for i in range(len(Tables.PRODUCTION[self.procedure])):
@@ -46,30 +46,30 @@ class Diagram:
                         return i
 
     def get_value(self):
-        return Tables.PRODUCTION[self.procedure][self.branch][self.level]
+        return Tables.PRODUCTION[self.procedure][self.branch][self.state]
 
     def move_forward(self):
-        if self.level < len(Tables.PRODUCTION[self.procedure][self.branch]) - 1:
-            self.level += 1
+        if self.state < len(Tables.PRODUCTION[self.procedure][self.branch]) - 1:
+            self.state += 1
             return True
         return False
 
 
 token = scanner.get_next_token()
-level = Diagram('Program')
+state = Diagram('Program')
 root = Node('Program')
 
 
 def move_to_next_state():
-    global level
+    global state
     global root
 
-    while not level.move_forward():
-        level = STATE_STACK.pop()
+    while not state.move_forward():
+        state = STATE_STACK.pop()
         root = root.parent
 
 while True:
-    current_state = level.get_value()
+    current_state = state.get_value()
 
     if current_state == 'EPSILON':
         Child = Node('epsilon', parent= root)
@@ -78,7 +78,7 @@ while True:
 
     elif is_terminal(current_state):
         if token.value == current_state or token.type == current_state:
-            # matches. move both level and token
+            # matches. move both state and token
             if token.value == '$':
                 child = Node('$', parent= root)
                 break
@@ -86,25 +86,25 @@ while True:
             token = scanner.get_next_token()
             move_to_next_state()
         else:
-            # missing token. don't change token and move level
+            # missing token. don't change token and move state
             syntax_errors.append(f"#{scanner.line_num} : syntax error, missing {current_state}")
             move_to_next_state()
 
-    else: # non-terminal level
+    else: # non-terminal state
         if token.value in Tables.FIRST[current_state] or token.type in Tables.FIRST[current_state] or 'EPSILON' in Tables.FIRST[current_state]:
-            STATE_STACK.append(level)
-            level = Diagram(current_state)
+            STATE_STACK.append(state)
+            state = Diagram(current_state)
             child = Node(current_state, parent= root)
             root = child
 
         else:
             if token in Tables.FOLLOW[current_state]:
-                # missing procedure error. move level without changing the token
+                # missing procedure error. move state without changing the token
                 syntax_errors.append(f"#{scanner.line_num} : syntax error, missing {current_state}")
                 move_to_next_state()
 
             else:
-                # illegal procedure error. don't move level and change token
+                # illegal procedure error. don't move state and change token
                 if token.value == '$':
                     syntax_errors.append(f"#{scanner.line_num} : syntax error, Unexpected EOF")
                     break
