573b0bede466de963f790844cc2fd62140588231
Negarnbkhti42 <norob35@gmail.com>
2022-01-15 22:28:55 +0330

    addition to operation

--- code_gen.py
+++ code_gen.py
@@ -11,22 +11,27 @@ SYMBOL_TABLE={"a": {
 operations_dict={ "+":"ADD", "-":"SUB", "*":"MULT", "=":"ASSIGN", "<":"LT", "==":"EQ" }
 
 
-def add_id(id):
+def pid(id):
     SEMANTIC_STACK.append(SYMBOL_TABLE[id]["address"])
+
+def pnum(num):
+    SEMANTIC_STACK.append(f"#{num}")
     
 def save():
     pass
 
 
 def assign():
-    pass
+    A1 = SEMANTIC_STACK.pop()
+    R = SEMANTIC_STACK.pop()
+    PROGRAM_BLOCK.append(f"(ASSIGN, {A1}, {R})")
 
-def jump():
-    pass
+def jump(destination):
+    PROGRAM_BLOCK.append(f"(JP, {destination}, , )")
 
 def jump_false(destination):
     operand = SEMANTIC_STACK.pop()
-    PROGRAM_BLOCK.append(f"JPF, {operand}, {destination}")
+    PROGRAM_BLOCK.append(f"(JPF, {operand}, {destination})")
 
 def execute_operation():
     operand_2 = SEMANTIC_STACK.pop()
@@ -39,25 +44,19 @@ def execute_operation():
 
 
 ACTION_SIGN={
-    "#pid": add_id,
+    "#pid": pid,
+    "#pnum": pnum,
     "#save": save,
     "#assign": assign,
     "#op_exec": execute_operation,
-    "#jmp": jump,
-    "#jmpf": jump_false,
+    "#jp": jump,
+    "#jpf": jump_false,
     "#addop": ""
 }
 
 
-inputTXT=input()
-
-if input.contain(operations_dict.keys()):
-    pass
-
-# while a*b/c-d+r 
-
 def generate_code(action, token):
-    ACTION_SIGN[action](token)
-
-
-
+    if token:
+        ACTION_SIGN[action](token)
+    else:
+        ACTION_SIGN[action]()
