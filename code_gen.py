0341dfec9ff8b3a8d32583a474fa4015bf4815f9
Negarnbkhti42 <norob35@gmail.com>
2022-02-03 09:11:28 +0330

    output handle

--- code_gen.py
+++ code_gen.py
@@ -8,8 +8,9 @@ BREAK_LIST = []
 PROGRAM_BLOCK_INDEX = 0
 TEMPORARY = 2000
 
-operations_dict = {"+": "ADD", "-": "SUB", "*": "MULT", "<": "LT", "==": "EQ"}
+NUM_OF_ARGS = 0
 
+operations_dict = {"+": "ADD", "-": "SUB", "*": "MULT", "<": "LT", "==": "EQ"}
 
 
 def add_type(type):
@@ -33,7 +34,8 @@ def declare_arr(param=0):
     type = SEMANTIC_STACK.pop()
     addr = get_temp()
     length = get_temp(int(size))
-    symbol_table.SYMBOL_TABLE.append(symbol_table.Symbol(id, 'array', addr, attr= size))
+    symbol_table.SYMBOL_TABLE.append(
+        symbol_table.Symbol(id, 'array', addr, attr=size))
 
 
 def func_id(param=None):
@@ -47,32 +49,34 @@ def func_id(param=None):
     symbol_table.SYMBOL_TABLE.append("func_start")
 
 
-def label(param= None):
+def label(param=None):
     SEMANTIC_STACK.append(PROGRAM_BLOCK_INDEX)
 
 
-def set_func_temps(param= None):
+def set_func_temps(param=None):
     for i in range(2):
         SEMANTIC_STACK.append(get_temp())
 
-def start_func(param= None):
+
+def start_func(param=None):
     RETURN_LIST.append(('begin_func', '#0'))
 
 
-def end_func(param= None):
+def end_func(param=None):
     index = RETURN_LIST.pop()
     while index[0] != 'begin_func':
         add_to_pb(index[0], 'ASSIGN', index[1], SEMANTIC_STACK[-1])
         add_to_pb(index[0]+1, 'JP', PROGRAM_BLOCK_INDEX)
         index = RETURN_LIST.pop()
 
-def return_address(param= None):
+
+def return_address(param=None):
     global PROGRAM_BLOCK_INDEX
 
     name = SEMANTIC_STACK[-4]
     if name == 'main':
         return
-    addr= SEMANTIC_STACK[-2]
+    addr = SEMANTIC_STACK[-2]
     add_to_pb(PROGRAM_BLOCK_INDEX, 'JP', f"@{addr}")
     PROGRAM_BLOCK_INDEX += 1
 
@@ -84,31 +88,33 @@ def declare_func(param=None):
     attr_type = []
     symbol = symbol_table.SYMBOL_TABLE.pop()
     while symbol != 'func_start':
-        attr.append(symbol.addr)
+        attr.append(symbol.address)
         attr_type.append(symbol.type)
         symbol = symbol_table.SYMBOL_TABLE.pop()
     attr.append(SEMANTIC_STACK[-3])
     attr.reverse()
     attr.append(SEMANTIC_STACK[-2])
     attr.append(SEMANTIC_STACK[-1])
-    symbol_table.SYMBOL_TABLE.append(symbol_table.Symbol(SEMANTIC_STACK[-4], 'func', attr, attr= attr))
-    for i in range(4): 
+    symbol_table.SYMBOL_TABLE.append(symbol_table.Symbol(
+        SEMANTIC_STACK[-4], 'func', attr, attr=attr))
+    for i in range(4):
         SEMANTIC_STACK.pop()
     # TODO: handle main
+
 
 def add_id(id):
     SEMANTIC_STACK.append(id.value)
 
 
 def pid(id):
-    SEMANTIC_STACK.append(symbol_table.find_addr(id.value))
+    SEMANTIC_STACK.append('print' if id.value ==
+                          'output' else symbol_table.find_addr(id.value))
 
 
 def save(param=None):
     global PROGRAM_BLOCK_INDEX
 
     SEMANTIC_STACK.append(PROGRAM_BLOCK_INDEX)
-    add_to_pb(PROGRAM_BLOCK_INDEX, 'JPF', '')
     PROGRAM_BLOCK_INDEX += 1
 
 
@@ -117,8 +123,9 @@ def assign(param=None):
 
     A1 = SEMANTIC_STACK.pop()
     R = SEMANTIC_STACK.pop()
-    add_to_pb(PROGRAM_BLOCK_INDEX, 'ASSIGN', A1, r= R)
+    add_to_pb(PROGRAM_BLOCK_INDEX, 'ASSIGN', A1, r=R)
     PROGRAM_BLOCK_INDEX += 1
+
 
 def jump(param=None):
     address = SEMANTIC_STACK.pop()
@@ -130,7 +137,7 @@ def jump_false(param=None):
 
     address = SEMANTIC_STACK.pop()
     value = SEMANTIC_STACK.pop()
-    add_to_pb(address, 'JPF', value, op2= PROGRAM_BLOCK_INDEX)
+    add_to_pb(address, 'JPF', value, op2=PROGRAM_BLOCK_INDEX)
 
 
 def jump_false_iter(param=None):
@@ -138,13 +145,14 @@ def jump_false_iter(param=None):
     # TODO: return later
     value = SEMANTIC_STACK.pop()
     address = SEMANTIC_STACK.pop()
-    add_to_pb(PROGRAM_BLOCK_INDEX, 'JPF', value, op2= address)
+    add_to_pb(PROGRAM_BLOCK_INDEX, 'JPF', value, op2=address)
+    PROGRAM_BLOCK_INDEX += 1
 
 
 def jump_false_save(param=None):
     address = SEMANTIC_STACK.pop()
     value = SEMANTIC_STACK.pop()
-    add_to_pb(address, 'JPF', value, op2= PROGRAM_BLOCK_INDEX + 1)
+    add_to_pb(address, 'JPF', value, op2=PROGRAM_BLOCK_INDEX + 1)
     save()
 
 
@@ -155,7 +163,8 @@ def execute_operation(param=None):
     operator = SEMANTIC_STACK.pop()
     operand_1 = SEMANTIC_STACK.pop()
     result = get_temp()
-    add_to_pb(PROGRAM_BLOCK_INDEX, operator, operand_1, op2= operand_2, r= result)
+    add_to_pb(PROGRAM_BLOCK_INDEX, operator,
+              operand_1, op2=operand_2, r=result)
     PROGRAM_BLOCK_INDEX += 1
     SEMANTIC_STACK.append(result)
 
@@ -164,10 +173,9 @@ def addop(operator):
     SEMANTIC_STACK.append(operations_dict[operator.value])
 
 
-
 def set_index(param=None):
     global PROGRAM_BLOCK_INDEX
-    
+
     index = SEMANTIC_STACK.pop()
     id = SEMANTIC_STACK.pop()
     temp = get_temp()
@@ -179,37 +187,38 @@ def set_index(param=None):
     SEMANTIC_STACK.append(f"@{temp}")
 
 
-def break_start(param= None):
+def break_start(param=None):
     BREAK_LIST.append('loop_begin')
 
 
-def break_op(param= None):
+def break_op(param=None):
     global PROGRAM_BLOCK_INDEX
 
     if len(BREAK_LIST) == 0:
-        SEMANTIC_ERROR.append(f"#{param.line}: Semantic Error! No 'repeat ... until' found for 'break'")
+        SEMANTIC_ERROR.append(
+            f"#{param.line}: Semantic Error! No 'repeat ... until' found for 'break'")
         return
 
     BREAK_LIST.append(PROGRAM_BLOCK_INDEX)
     PROGRAM_BLOCK_INDEX += 1
 
 
-def break_end(param= None):
-    index= BREAK_LIST.pop()
-    
+def break_end(param=None):
+    index = BREAK_LIST.pop()
+
     while index != 'loop_begin':
         add_to_pb(index, 'JP', PROGRAM_BLOCK_INDEX)
         index = BREAK_LIST.pop()
 
 
-def return_op(param= None):
+def return_op(param=None):
     global PROGRAM_BLOCK_INDEX
 
     RETURN_LIST.append((PROGRAM_BLOCK_INDEX, '#0'))
     PROGRAM_BLOCK_INDEX += 2
 
 
-def return_value_op(param= None):
+def return_value_op(param=None):
     global PROGRAM_BLOCK_INDEX
 
     value = SEMANTIC_STACK.pop()
@@ -217,8 +226,59 @@ def return_value_op(param= None):
     PROGRAM_BLOCK_INDEX += 2
 
 
-def set_arg_pointer(param=None):
-    SEMANTIC_STACK.append('_arg_pointer')
+def increase_arg_given(param=None):
+    global NUM_OF_ARGS
+    NUM_OF_ARGS += 1
+
+
+def func_call(param):
+    global PROGRAM_BLOCK_INDEX
+    global SEMANTIC_STACK
+    global SEMANTIC_ERROR
+
+    if len(SEMANTIC_STACK) > 1 and SEMANTIC_STACK[-2] == 'print':
+        add_to_pb(PROGRAM_BLOCK_INDEX, 'PRINT', SEMANTIC_STACK[-1])
+        for _ in range(2):
+            SEMANTIC_STACK.pop()
+
+        PROGRAM_BLOCK_INDEX += 1
+        NUM_OF_ARGS = 0
+        return
+
+    argLen = len(SEMANTIC_STACK)
+    funcionArgs = []
+
+    for symbol in SEMANTIC_STACK[::-1]:
+        if isinstance(symbol, list):
+            funcionArgs = symbol
+            break
+
+    originalArgLen = len(funcionArgs) - 3
+    func = symbol_table.getSymbol(funcionArgs)
+
+    if originalArgLen != NUM_OF_ARGS:
+        SEMANTIC_ERROR.append(
+            f"#{param.line}: semantic error! Mismatch in numbers of arguments of '{func.id}'")
+        for i in range(NUM_OF_ARGS):
+            SEMANTIC_STACK.pop()
+        return
+
+    for i in range(NUM_OF_ARGS):
+        formal_arg = SEMANTIC_STACK[argLen - NUM_OF_ARGS + i]
+        original_arg = funcionArgs[i + 1]
+
+        formal_arg_type = symbol_table.getSymbol(formal_arg).type
+        original_arg_type = func.attr[i]
+
+    if formal_arg_type != original_arg_type:
+        SEMANTIC_ERROR.append(
+            f"#{param.line}: Semantic Error! Mismatch in type of argument {i+1} for '{func.id}'. Expected '{original_arg_type}' but got '{formal_arg_type}' instead")
+    add_to_pb(PROGRAM_BLOCK_INDEX, 'ASSIGN', formal_arg, r=original_arg)
+    PROGRAM_BLOCK_INDEX += 1
+
+    for i in range(NUM_OF_ARGS+1):
+        SEMANTIC_STACK.pop()
+    NUM_OF_ARGS = 0
 
 
 ACTION_SIGN = {
@@ -249,8 +309,8 @@ ACTION_SIGN = {
     "#break_end": break_end,
     "#return": return_op,
     "#return_value": return_value_op,
-    "#set_arg_pointer": set_arg_pointer,
-    "#func_call": ''
+    "#increase_arg_given": increase_arg_given,
+    "#func_call": func_call
 }
 
 
@@ -263,6 +323,7 @@ def get_temp(size=1):
     addr = TEMPORARY
     TEMPORARY += 4*size
     return addr
+
 
 def add_to_pb(index, action, op1, op2='', r=''):
     global PROGRAM_BLOCK
--- symbol_table.py
+++ symbol_table.py
@@ -1,14 +1,13 @@
-SYMBOL_TABLE=[]
+SYMBOL_TABLE = []
 
 
 class Symbol:
     def __init__(self, id, type, address, attr=0):
-        
+
         self.id = id
         self.address = address
-        self.type=type
-        self.attr=attr
-
+        self.type = type
+        self.attr = attr
 
 
 def find_addr(id):
@@ -17,3 +16,11 @@ def find_addr(id):
             continue
         if symbol.id == id:
             return symbol.address
+
+
+def getSymbol(addr):
+    for symbol in SYMBOL_TABLE[::-1]:
+        if type(symbol) == str:
+            continue
+        if symbol.address == addr:
+            return symbol
